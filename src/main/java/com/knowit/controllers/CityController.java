bcb53d5ab671bf8b009b8e2fe1d06395e2f2fbd5
Hari Om Sundram <75016666+HariOmSundram@users.noreply.github.com>
2025-01-03 01:06:39 +0530

    Moderate Changes

--- src/main/java/com/knowit/controllers/CityController.java
+++ src/main/java/com/knowit/controllers/CityController.java
@@ -11,7 +11,6 @@ import com.knowit.services.CitiesSevice;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
